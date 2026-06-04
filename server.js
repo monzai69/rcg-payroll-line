344fdc32639786ffd1076eb450166e057e05f761
monzai69 <zmonss@gmail.com>
2026-06-04 11:29:01 +0700

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const admin = require('firebase-admin');
 const path = require('path');
 
 // ── FIREBASE INIT ──────────────────────────────────────
-const serviceAccount = require('./serviceAccount.json');
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
